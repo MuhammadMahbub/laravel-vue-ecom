186d47ff646da969658f8f1174896da1c6dfe6de
Muhammad Mahbub <muhammadmahbub07@gmail.com>
2024-03-17 04:56:16 +0600

    components separate

---
+++ frontend/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as CartSideBar } from "./CartSideBar.vue";
+export { default as FooterPart } from "./FooterPart.vue";
+export { default as HeaderPart } from "./HeaderPart.vue";
+export { default as LoginModal } from "./LoginModal.vue";
+// export { default as LoginForm } from "./LoginForm.vue";
+export { default as MobileMenu } from "./MobileMenu.vue";
+export { default as NavBar } from "./NavBar.vue";
+export { default as NewsLetter } from "./NewsLetter.vue";
